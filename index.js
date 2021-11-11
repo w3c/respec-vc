da58022f48e4816d75f189ea5e22281b39a535fe
Manu Sporny <msporny@digitalbazaar.com>
2021-11-10 19:47:47 -0500

    Add complete working implementation for LDI and JWT.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import vc from '@digitalbazaar/vc';
 import contexts from '@digitalbazaar/vc/lib/contexts';
 import {extendContextLoader} from 'jsonld-signatures';
 import ed25519Context from 'ed25519-signature-2020-context';
+import * as jose from 'jose';
 import {Ed25519VerificationKey2020} from
   '@digitalbazaar/ed25519-verification-key-2020';
 import {Ed25519Signature2020} from '@digitalbazaar/ed25519-signature-2020';
@@ -21,10 +22,92 @@ const documentLoader = extendContextLoader(async function documentLoader(url) {
   throw new Error(`Document loader unable to load URL "${url}".`);
 });
 
+async function attachJwt({credential, iss, jwk}) {
+  const jwt = new jose.SignJWT({vc: credential})
+    .setProtectedHeader({ alg: 'ES256' })
+    .setIssuedAt()
+    .setIssuer(iss)
+    .sign(jwk.privateKey);
+
+  return jwt;
+};
+
+async function attachProof({credential, suite}) {
+  const credentialCopy = JSON.parse(JSON.stringify(credential));
+  return vc.issue({credential: credentialCopy, suite, documentLoader});
+};
+
+async function createVcExamples() {
+  // generate base keypair and signature suite
+  const keyPair = await Ed25519VerificationKey2020.generate();
+  const suite = new Ed25519Signature2020({
+    key: keyPair
+  });
+  const jwk = await jose.generateKeyPair('ES256');
+
+  // process every example that needs a vc-proof
+  const vcProofExamples = document.querySelectorAll(".vc");
+  for(const example of vcProofExamples) {
+    const verificationMethod =
+      example.getAttribute('data-vc-verification-method');
+    suite.verificationMethod =
+      verificationMethod || 'did:key:' + keyPair.publicKey;
+
+    // extract and sign the example
+    const originalText = example.innerHTML;
+    let credential = {};
+    try {
+      let exampleText = example.innerText;
+      exampleText = exampleText.replace(/\/\/ .*$/gm, '');
+      credential = JSON.parse(exampleText);
+    } catch(e) {
+      console.error('respec-vc error: Failed to create Verifiable Credential.',
+        e, example.innerText);
+      continue;
+    }
+
+    const verifiableCredentialProof = await attachProof({credential, suite});
+    const verifiableCredentialJwt =
+      await attachJwt({credential, iss: suite.verificationMethod, jwk});
+
+    // set up the unsigned button action
+    const unsignedButton = document.createElement('button');
+    unsignedButton.innerText = 'Credential';
+    unsignedButton.onclick = () => {
+      example.innerHTML = originalText;
+    };
+
+    // set up the signed proof button action
+    const signedProofButton = document.createElement('button');
+    signedProofButton.innerText = 'Verifiable Credential (with proof)';
+    signedProofButton.onclick = () => {
+      example.innerText = JSON.stringify(verifiableCredentialProof, null, 2)
+        .match(/.{1,75}/g).join('\n');
+    };
+
+    // set up the signed JWT button action
+    const signedJwtButton = document.createElement('button');
+    signedJwtButton.innerText = 'Verifiable Credential (as JWT)';
+    signedJwtButton.onclick = () => {
+      example.innerText = verifiableCredentialJwt.match(/.{1,75}/g).join('\n');
+    };
+
+    // set up the tab separator
+    const tabSeparator = document.createElement('div');
+    tabSeparator.style.background = 'gray';
+    tabSeparator.style.height = '1px';
+    tabSeparator.style.border = '1px';
+    tabSeparator.style.margin = '-2px 0px';
+
+    // prepend the buttons before the preformatted example
+    example.before(unsignedButton);
+    example.before(signedProofButton);
+    example.before(signedJwtButton);
+    example.before(tabSeparator);
+  }
+}
+
 // setup exports on window
 window.respecVc = {
-  Ed25519VerificationKey2020,
-  Ed25519Signature2020,
-  documentLoader,
-  vc
+  createVcExamples
 }
