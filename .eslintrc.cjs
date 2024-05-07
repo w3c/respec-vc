da959db049d89943fcff71eea25123d76a4a4e8e
Benjamin Young <byoung@digitalbazaar.com>
2024-05-07 15:05:21 -0400

    Add eslint.

---
+++ .eslintrc.cjs
@@ -0,0 +1,26 @@
+/*
+ * Copyright 2024 Digital Bazaar, Inc.
+ *
+ * SPDX-License-Identifier: LicenseRef-w3c-3-clause-bsd-license-2008 OR LicenseRef-w3c-test-suite-license-2023
+ */
+
+module.exports = {
+  env: {
+    browser: true
+  },
+  ignorePatterns: ["dist/*", "webpack.config.js"],
+  extends: [
+    'eslint-config-digitalbazaar',
+    'eslint-config-digitalbazaar/jsdoc',
+    'digitalbazaar/module',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  rules: {
+    'jsdoc/check-examples': 0,
+    'max-len': ['error', {ignorePattern: '\\* SPDX-License-Identifier: ',
+      ignoreUrls: true}]
+  }
+};
