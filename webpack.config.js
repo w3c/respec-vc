41296f7a6f94e486c4dccddf3bf96c38df9c01cd
gabe <gcohen@squareup.com>
2024-06-03 21:31:06 -0700

    add back proper polyfills

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,24 @@
 const path = require('path');
-const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = [{
   mode: 'development',
+  entry: './index.js',
   plugins: [
-    new NodePolyfillPlugin()
+    new webpack.ProvidePlugin({
+      process: 'process/browser.js',
+      Buffer: ['buffer', 'Buffer'],
+    }),
   ],
   watch: true,
+  resolve: {
+    fallback: {
+      buffer: require.resolve("buffer/"),
+      crypto: require.resolve("crypto-browserify"),
+      stream: require.resolve("stream-browserify"),
+      vm: require.resolve("vm-browserify"),
+    },
+  },
   module: {
     rules: [
       {
