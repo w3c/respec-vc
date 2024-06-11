6df8a2467d91a29ff52a0ea75f7b5a9d74a60938
gabe <gcohen@squareup.com>
2024-06-10 19:31:19 -0700

    merge

--- index.js
+++ index.js
@@ -1,30 +1,46 @@
+import * as bbs2023Cryptosuite from '@digitalbazaar/bbs-2023-cryptosuite';
+import * as Bls12381Multikey from '@digitalbazaar/bls12-381-multikey';
 import * as EcdsaMultikey from '@digitalbazaar/ecdsa-multikey';
+import * as ecdsaRdfc2019Cryptosuite from
+    '@digitalbazaar/ecdsa-rdfc-2019-cryptosuite';
 import * as ecdsaSd2023Cryptosuite
   from '@digitalbazaar/ecdsa-sd-2023-cryptosuite';
 import * as Ed25519Multikey from '@digitalbazaar/ed25519-multikey';
 import * as examples1Context from '@digitalbazaar/credentials-examples-context';
+import * as jose from 'jose';
+import * as mfHasher from 'multiformats/hashes/hasher';
 import * as odrlContext from '@digitalbazaar/odrl-context';
+import {base64pad, base64url} from 'multiformats/bases/base64';
 import {defaultDocumentLoader, issue} from '@digitalbazaar/vc';
 import {extendContextLoader, purposes} from 'jsonld-signatures';
 import {getCoseHtml, getJoseHtml, getSdJwtHtml} from './src/html';
+import {sha3_256, sha3_384} from '@noble/hashes/sha3';
+import {base16} from 'multiformats/bases/base16';
+import {base58btc} from 'multiformats/bases/base58';
 import {DataIntegrityProof} from '@digitalbazaar/data-integrity';
 import ed25519Context from 'ed25519-signature-2020-context';
 import {Ed25519Signature2020} from '@digitalbazaar/ed25519-signature-2020';
-import {
-  Ed25519VerificationKey2020,
-} from '@digitalbazaar/ed25519-verification-key-2020';
-import {
-  cryptosuite as eddsaRdfc2022CryptoSuite,
-} from '@digitalbazaar/eddsa-rdfc-2022-cryptosuite';
+import {Ed25519VerificationKey2020} from
+  '@digitalbazaar/ed25519-verification-key-2020';
+import {cryptosuite as eddsaRdfc2022CryptoSuite} from
+  '@digitalbazaar/eddsa-rdfc-2022-cryptosuite';
 import examples2Context from './contexts/credentials/examples/v2';
 import {getCoseExample} from './src/cose';
 import {getJoseExample} from './src/jose';
 import {getSdJwtExample} from './src/sd-jwt';
-import {privateKey} from './src/common';
+import {sha256} from '@noble/hashes/sha256';
+import {sha384} from '@noble/hashes/sha512';
 
 // default types
-const TAB_TYPES = ['ecdsa-sd-2023', 'eddsa-rdfc-2022',
-  'jose', 'sd-jwt', 'cose'];
+const TAB_TYPES = [
+  'ecdsa-rdfc-2019',
+  'eddsa-rdfc-2022',
+  'ecdsa-sd-2023',
+  'bbs-2023',
+  'jose',
+  'sd-jwt',
+  'cose',
+];
 // additional types: Ed25519Signature2020
 
 // purposes used below
@@ -51,6 +67,84 @@ const documentLoader = extendContextLoader(async function documentLoader(url) {
   }
   return defaultDocumentLoader(url);
 });
+
+async function createBBSExampleProof() {
+  const key = await Bls12381Multikey.generateBbsKeyPair({
+    algorithm: Bls12381Multikey.ALGORITHMS.BBS_BLS12381_SHA256,
+  });
+
+  const proof = new DataIntegrityProof({
+    signer: key.signer(),
+    cryptosuite: bbs2023Cryptosuite.createSignCryptosuite({
+      mandatoryPointers: ['/issuer'],
+    }),
+  });
+
+  return {
+    proof,
+    key,
+    label: 'bbs',
+  };
+}
+
+async function createEcdsaRdfc2019ExampleProof() {
+  const key = await EcdsaMultikey
+    .generate({curve: 'P-256'});
+
+  // ecdsa-rdfc-2019
+  const {cryptosuite: rdfcCryptosuite} = ecdsaRdfc2019Cryptosuite;
+  const proof = new DataIntegrityProof({
+    signer: key.signer(),
+    cryptosuite: rdfcCryptosuite,
+  });
+
+  return {
+    proof,
+    key,
+    label: 'ecdsa',
+  };
+}
+
+async function createEcdsaSd2023ExampleProof() {
+  const key = await EcdsaMultikey
+    .generate({curve: 'P-256'});
+
+  // ecdsa-sd-2023
+  const {createSignCryptosuite} = ecdsaSd2023Cryptosuite;
+  const proof = new DataIntegrityProof({
+    signer: key.signer(),
+    cryptosuite: createSignCryptosuite({
+      mandatoryPointers: ['/issuer'],
+    }),
+  });
+
+  return {
+    proof,
+    key,
+    label: 'ecdsa-sd',
+  };
+}
+
+async function createEddsaRdfc2022ExampleProof() {
+  // Ed25519Signature2020
+  const keyPairEd25519VerificationKey2020 = await Ed25519VerificationKey2020
+    .generate();
+
+  const key = await Ed25519Multikey
+    .from(keyPairEd25519VerificationKey2020);
+
+  // eddsa-rdfc-2022
+  const proof = new DataIntegrityProof({
+    signer: key.signer(),
+    cryptosuite: eddsaRdfc2022CryptoSuite,
+  });
+
+  return {
+    proof,
+    key,
+    label: 'eddsa',
+  };
+}
 
 async function attachProof({credential, suite}) {
   const credentialCopy = JSON.parse(JSON.stringify(credential));
@@ -115,23 +209,34 @@ function addVcExampleStyles() {
     color: #333;
   }
 
+  .vc-tab-content {
+    display: none;
+  }
+
+  ${radioLabels.join(',\n  ')} {
+    border-bottom-color: #fff;
+    background: #fff;
+    color: #222;
+  }
+
+  ${radioSelector.join(',\n  ')} {
+    display: block;
+  }
+
   .vc-jose-cose-tabbed, .vc-jose-cose-tabbed-jwt, .vc-jose-cose-tabbed-sd-jwt, .vc-jose-cose-tabbed-cose,
   .sd-jwt-tabbed {
     overflow-x: hidden;
     margin: 0 0;
   }
-
   .vc-jose-cose-tabbed h1, .vc-jose-cose-jwt-tabbed h1, .vc-jose-cose-sd-jwt-tabbed h1, .vc-jose-cose-cose-tabbed h1,
   .sd-jwt-tabbed h1 {
     font-size: 1em;
     margin: 0 0;
   }
-
   .vc-jose-cose-tabbed [type="radio"], .vc-jose-cose-tabbed-jwt [type="radio"], .vc-jose-cose-tabbed-sd-jwt [type="radio"], .vc-jose-cose-tabbed-cose [type="radio"],
   .sd-jwt-tabbed [type="radio"] {
     display: none;
   }
-
   .vc-jose-cose-tabs, .vc-jose-cose-jwt-tabs, .vc-jose-cose-sd-jwt-tabs, .vc-jose-cose-cose-tabs,
   .sd-jwt-tabs {
     display: flex;
@@ -140,13 +245,11 @@ function addVcExampleStyles() {
     padding: 0;
     border-bottom: 1px solid #ccc;
   }
-
   li.vc-jose-cose-tab, li.vc-jose-cose-jwt-tab, li.vc-jose-cose-sd-jwt-tab, li.vc-jose-cose-cose-tab,
   li.sd-jwt-tab {
     margin: 0 0;
     margin-left: 8px;
   }
-
   .vc-jose-cose-tab>label, .vc-jose-cose-jwt-tab>label, .vc-jose-cose-sd-jwt-tab>label, .vc-jose-cose-cose-tab>label,
   .sd-jwt-tab>label {
     display: block;
@@ -160,7 +263,6 @@ function addVcExampleStyles() {
     cursor: pointer;
     transition: all 0.3s;
   }
-
   .vc-jose-cose-tab:hover label, .vc-jose-cose-jwt-tab:hover label, .vc-jose-cose-sd-jwt-tab:hover label, .vc-jose-cose-cose-tab:hover label,
   .sd-jwt-tab:hover label {
     border-left-color: #333;
@@ -168,12 +270,10 @@ function addVcExampleStyles() {
     border-right-color: #333;
     color: #333;
   }
-
   .vc-jose-cose-tab-content,
   .sd-jwt-tab-content {
     display: none;
   }
-
   .vc-jose-cose-tabbed [type="radio"]:nth-of-type(1):checked~.vc-jose-cose-tabs .vc-jose-cose-tab:nth-of-type(1) label,
   .vc-jose-cose-tabbed [type="radio"]:nth-of-type(2):checked~.vc-jose-cose-tabs .vc-jose-cose-tab:nth-of-type(2) label,
   .vc-jose-cose-tabbed [type="radio"]:nth-of-type(3):checked~.vc-jose-cose-tabs .vc-jose-cose-tab:nth-of-type(3) label,
@@ -184,7 +284,6 @@ function addVcExampleStyles() {
     background: #fff;
     color: #222;
   }
-
   .vc-jose-cose-tabbed [type="radio"]:nth-of-type(1):checked~.vc-jose-cose-tab-content:nth-of-type(1),
   .vc-jose-cose-tabbed [type="radio"]:nth-of-type(2):checked~.vc-jose-cose-tab-content:nth-of-type(2),
   .vc-jose-cose-tabbed [type="radio"]:nth-of-type(3):checked~.vc-jose-cose-tab-content:nth-of-type(3),
@@ -193,48 +292,39 @@ function addVcExampleStyles() {
   .sd-jwt-tabbed [type="radio"]:nth-of-type(3):checked~.sd-jwt-tab-content:nth-of-type(3) {
     display: block;
   }
-
   .sd-jwt-header, .jwt-header, .vc-jose-cose-jwt .header, .vc-jose-cose-sd-jwt .header, .vc-jose-cose-cose .header {
     color: red;
   }
   .sd-jwt-payload, .jwt-payload, .vc-jose-cose-jwt .payload, .vc-jose-cose-sd-jwt .payload, .vc-jose-cose-cose .payload {
     color: green;
   }
-
   .sd-jwt-signature, .jwt-signature, .vc-jose-cose-jwt .signature, .vc-jose-cose-sd-jwt .signature, .vc-jose-cose-cose .signature {
     color: blue;
   }
-
   .sd-jwt-disclosure, .vc-jose-cose-jwt .disclosure, .vc-jose-cose-sd-jwt .disclosure, .vc-jose-cose-cose .disclosure {
     color: purple;
   }
-
   .sd-jwt-compact, .jwt-compact, .vc-jose-cose-jwt .compact, .vc-jose-cose-sd-jwt .compact, .vc-jose-cose-cose .compact {
     background-color: rgba(0,0,0,.03);
   }
-
   .cose-text, .jose-text, .vc-jose-cose-jwt .text, .vc-jose-cose-sd-jwt .text, .vc-jose-cose-cose .text {
     font-family: monospace;
     color: green;
   }
-
   .disclosure {
       margin: 10px 0;
       font-size: 12px;
       line-height: 1.6;
       padding: 5px;
   }
-
   .disclosure h3 {
       margin: 0;
       font-size: 14px;
       padding-left: 5px;
   }
-
   .disclosure .claim-name {
       color: #333;
   }
-
   .disclosure .hash,
   .disclosure .disclosure-value,
   .disclosure .contents {
@@ -242,12 +332,10 @@ function addVcExampleStyles() {
       word-wrap: break-word;
       display: inline;
   }
-
   .disclosure p {
       margin: 0;
       padding-left: 5px;
   }
-
   .disclosure pre {
       white-space: pre-wrap;
       word-wrap: break-word;
@@ -256,7 +344,6 @@ function addVcExampleStyles() {
       line-height: 1.6;
       display: inline-block;
   }
-
   .header-value {
       white-space: pre-wrap;
       word-wrap: break-word;
@@ -267,16 +354,6 @@ function addVcExampleStyles() {
   }
   .vc-tab-content {
     display: none;
-  }
-
-  ${radioLabels.join(',\n  ')} {
-    border-bottom-color: #fff;
-    background: #fff;
-    color: #222;
-  }
-
-  ${radioSelector.join(',\n  ')} {
-    display: block;
   }`;
 
   document.getElementsByTagName('head')[0].appendChild(exampleStyles);
@@ -287,30 +364,128 @@ function addContext(url, context) {
 }
 
 async function createVcExamples() {
-  // generate base keypair and signature suites
-  // ecdsa-sd-2023
-  const keyPairEcdsaMultikeyKeyPair = await EcdsaMultikey
-    .generate({curve: 'P-256'});
-  const {createSignCryptosuite} = ecdsaSd2023Cryptosuite;
-  const suiteEcdsaMultiKey = new DataIntegrityProof({
-    signer: keyPairEcdsaMultikeyKeyPair.signer(),
-    cryptosuite: createSignCryptosuite({
-      mandatoryPointers: ['/issuer'],
-    }),
+  // process all 'vc-hash' entries
+  const sha2256Hasher = mfHasher.from({
+    name: 'sha2-256',
+    code: 0x12,
+    encode: input => sha256(input),
   });
+  const sha2384Hasher = mfHasher.from({
+    name: 'sha2-384',
+    code: 0x20,
+    encode: input => sha384(input),
+  });
+  const sha3256Hasher = mfHasher.from({
+    name: 'sha3-256',
+    code: 0x16,
+    encode: input => sha3_256(input),
+  });
+  const sha3384Hasher = mfHasher.from({
+    name: 'sha3-384',
+    code: 0x15,
+    encode: input => sha3_384(input),
+  });
+
+  const vcHashEntries = document.querySelectorAll('.vc-hash');
+  for(const hashEntry of vcHashEntries) {
+
+    // get the hash requirements
+    const hashUrl = hashEntry.dataset?.hashUrl || 'INVALID_URL';
+    const hashFormat = hashEntry.dataset?.hashFormat?.split(/(\s+)/) || [];
+    let encodedHash = null;
+
+    // select the base encoder (default: base64-url with no padding)
+    let baseEncoder;
+    if(hashFormat.includes('sri')) {
+      baseEncoder = base64pad;
+    } else if(hashFormat.includes('base16')) {
+      baseEncoder = base16;
+    } else if(hashFormat.includes('base58btc')) {
+      baseEncoder = base58btc;
+    } else {
+      baseEncoder = base64url;
+    }
+
+    // retrieve the file and generate the hash
+    try {
+      const response = await fetch(hashUrl);
+
+      // ensure retrieval succeeded
+      if(response.status !== 200) {
+        throw new Error('Failed to retrieve ' + hashUrl);
+      }
+      const hashData = new Uint8Array(await response.arrayBuffer());
+
+      // determine the hash algorithm to use and produce the output accordingly
+      if(hashFormat.includes('openssl') && hashFormat.includes('-sha256')) {
+        const mfHash = await sha2256Hasher.digest(hashData);
+        encodedHash = Array.prototype.map.call(mfHash.digest, byte => {
+          return ('0' + (byte & 0xFF).toString(16)).slice(-2);
+        }).join('');
+      } else if(hashFormat.includes('sri')) {
+        if(hashFormat.includes('sha2-256')) {
+          const mfHash = await sha2256Hasher.digest(hashData);
+          encodedHash = 'sha256-' + baseEncoder.encode(mfHash.digest);
+        } else if(hashFormat.includes('sha2-384')) {
+          const mfHash = await sha2384Hasher.digest(hashData);
+          encodedHash = 'sha384-' + baseEncoder.encode(mfHash.digest);
+        }
+      } else if(hashFormat.includes('multihash')) {
+        if(hashFormat.includes('sha2-256')) {
+          const mfHash = await sha2256Hasher.digest(hashData).bytes;
+          encodedHash = baseEncoder.encode(mfHash);
+        } else if(hashFormat.includes('sha2-384')) {
+          const mfHash = await sha2384Hasher.digest(hashData).bytes;
+          encodedHash = baseEncoder.encode(mfHash);
+        } else if(hashFormat.includes('sha3-256')) {
+          const mfHash = await sha3256Hasher.digest(hashData).bytes;
+          encodedHash = baseEncoder.encode(mfHash);
+        } else if(hashFormat.includes('sha3-384')) {
+          const mfHash = await sha3384Hasher.digest(hashData).bytes;
+          encodedHash = baseEncoder.encode(mfHash);
+        }
+      }
+
+      // set the encodedHash value
+      hashEntry.innerText = encodedHash || 'Unsupported hash format: \'' +
+        hashEntry.dataset?.hashFormat + '\'';
+    } catch(e) {
+      console.error('respec-vc error: Failed to create cryptographic hash.',
+        e, hashEntry);
+      hashEntry.innerText = 'Error generating cryptographic hash for ' +
+        hashUrl;
+      continue;
+    }
+  }
+
+  // process all 'vc' entries
+  const exampleProofs = [];
+
+  // ecdsa-rdfc-2019
+  const ecdsaRdfc2019 = await createEcdsaRdfc2019ExampleProof();
+  exampleProofs.push(ecdsaRdfc2019);
+
   // Ed25519Signature2020
   const keyPairEd25519VerificationKey2020 = await Ed25519VerificationKey2020
     .generate();
-  const keyPairEd25519Multikey = await Ed25519Multikey
-    .from(keyPairEd25519VerificationKey2020);
   const suiteEd25519Signature2020 = new Ed25519Signature2020({
     key: keyPairEd25519VerificationKey2020,
   });
+
   // eddsa-rdfc-2022
-  const suiteEd25519Multikey = new DataIntegrityProof({
-    signer: keyPairEd25519Multikey.signer(),
-    cryptosuite: eddsaRdfc2022CryptoSuite,
-  });
+  const eddsaRdfc2022 = await createEddsaRdfc2022ExampleProof();
+  exampleProofs.push(eddsaRdfc2022);
+
+  // ecdsa-sd-2023
+  const ecdsaSd2023 = await createEcdsaSd2023ExampleProof();
+  exampleProofs.push(ecdsaSd2023);
+
+  // bbs-2023
+  const bbs2023 = await createBBSExampleProof();
+  exampleProofs.push(bbs2023);
+
+  // vc-jwt
+  const jwk = await jose.generateKeyPair('ES256');
 
   // add styles for examples
   addVcExampleStyles();
@@ -359,9 +534,10 @@ async function createVcExamples() {
      *
      * @param {string} suffix - One of the TAB_TYPES values (or `unsigned`).
      * @param {string} labelText - Human readable label name.
+     * @param {string} tabText - Text to display on the tab.
      * @param {contentCallback} callback - Function which returns HTML.
      */
-    function addTab(suffix, labelText, callback) {
+    function addTab(suffix, labelText, tabText, callback) {
       const button = document.createElement('input');
       button.setAttribute('type', 'radio');
       button.setAttribute('id', `vc-tab${vcProofExampleIndex}${suffix}`);
@@ -375,8 +551,16 @@ async function createVcExamples() {
 
       const label = document.createElement('li');
       label.setAttribute('class', 'vc-tab');
-      label.innerHTML =
-        `<label for='${button.getAttribute('id')}'>${labelText}</label>`;
+
+      const tabLabel = document.createElement('label');
+      tabLabel.setAttribute('for', button.getAttribute('id'));
+
+      const abbr = document.createElement('abbr');
+      abbr.setAttribute('title', labelText);
+      abbr.innerText = tabText;
+
+      tabLabel.appendChild(abbr);
+      label.appendChild(tabLabel);
       tabLabels.appendChild(label);
 
       const content = document.createElement('div');
@@ -393,23 +577,25 @@ async function createVcExamples() {
       }
     }
 
-    /*
-     * Add a Data Integrity based proof example tab
+    /**
+     * Add a Data Integrity based proof example tab.
+     *
      * @global string verificationMethod
-     * @param object suite
+     * @param {object} suite - Suite object.
+     * @param {string} tabText - Text to display on the tab.
+     * @param {string | undefined} key - Optional key to use for the proof.
      */
-    async function addProofTab(suite) {
+    async function addProofTab(suite, tabText, key) {
       let verifiableCredentialProof;
       const label = suite?.cryptosuite || suite.type;
 
-      if(label === 'ecdsa-sd-2023') {
-        suite.verificationMethod = 'did:key:' + keyPairEcdsaMultikeyKeyPair
-          .publicKeyMultibase;
+      if(key) {
+        suite.verificationMethod = 'did:key:' + key.publicKeyMultibase;
       } else {
         suite.verificationMethod = verificationMethod;
       }
 
-      addTab(label, `Secured with Data Integrity (${label})`, async () => {
+      addTab(label, `Secured with Data Integrity - ${label}`, tabText, async () => {
         // attach the proof
         try {
           verifiableCredentialProof = await attachProof({credential, suite});
@@ -423,41 +609,50 @@ async function createVcExamples() {
       });
     }
 
+    function hasTab(identifier) {
+      return tabTypes.indexOf(identifier) > -1;
+    }
+
     // set up the unsigned button
-    addTab('unsigned', 'Verifiable Credential', () => example.outerHTML);
+    addTab(
+      'unsigned',
+      'Unsecured credential',
+      'Credential',
+      () => example.outerHTML,
+    );
 
-    if(tabTypes.indexOf(suiteEd25519Signature2020.type) > -1) {
-      await addProofTab(suiteEd25519Signature2020);
-    }
-    if(tabTypes.indexOf(suiteEd25519Multikey.cryptosuite) > -1) {
-      await addProofTab(suiteEd25519Multikey);
-    }
-    if(tabTypes.indexOf(suiteEcdsaMultiKey.cryptosuite) > -1) {
-      await addProofTab(suiteEcdsaMultiKey);
-    }
-
-    if(tabTypes.indexOf('jose') > -1) {
-      addTab('jose', 'Secured with JOSE', async () => {
-        const joseExample = await getJoseExample(privateKey, credential);
-        return getJoseHtml({joseExample});
-      });
+    for(const {proof, key, label} of exampleProofs) {
+      if(hasTab(proof.cryptosuite)) {
+        await addProofTab(proof, label, key);
+      }
     }
 
-    if(tabTypes.indexOf('sd-jwt') > -1) {
+    if(hasTab(suiteEd25519Signature2020.type)) {
+      await addProofTab(suiteEd25519Signature2020, 'Ed25519Signature2020');
+    }
+
+    if(hasTab('jose')) {
+      addTab('jose', 'Secured with JOSE', 'jose',
+        async () => {
+          const joseExample = await getJoseExample(jwk, credential);
+          return getJoseHtml({joseExample});
+        });
+    }
+
+    if(hasTab('sd-jwt')) {
       addTab('sd-jwt', 'Secured with SD-JWT', async () => {
-        // eslint-disable-next-line max-len
-        const sdJwtExample = await getSdJwtExample(vcProofExampleIndex, privateKey, credential);
+        const sdJwtExample =
+          await getSdJwtExample(vcProofExampleIndex, jwk, credential);
         return getSdJwtHtml({sdJwtExample});
       });
     }
 
-    if(tabTypes.indexOf('cose') > -1) {
+    if(hasTab('cose')) {
       addTab('cose', 'Secured with COSE', async () => {
-        const coseExample = await getCoseExample(privateKey, credential);
+        const coseExample = await getCoseExample(jwk, credential);
         return getCoseHtml({coseExample});
       });
     }
-
     // append the tabbed content
 
     // replace the original example with the tabbed content
