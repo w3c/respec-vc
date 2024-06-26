545d114a712e78b6795bea378c867f911662592c
gabe <gcohen@squareup.com>
2024-06-25 21:58:52 -0700

    some progress

--- src/sd-jwt.js
+++ src/sd-jwt.js
@@ -80,9 +80,8 @@ const getCredential = async (
   messageType,
   messageJson,
 ) => {
-  let oldMessageType = (messageType === 'application/vc+sd-jwt')
-    ? 'application/vc+ld+json+sd-jwt' : 'application/vp+ld+json+sd-jwt';
-
+  const oldMessageType = (messageType === 'application/vc-ld+sd-jwt') ?
+    'application/vc+ld+json+sd-jwt' : 'application/vp+ld+json+sd-jwt';
   return issuer({
     alg: privateKey.alg,
     type: oldMessageType,
@@ -98,7 +97,7 @@ const getPresentation = async (
   messageType,
   messageJson,
 ) => {
-  const mediaType = 'application/vc+sd-jwt';
+  const mediaType = 'application/vp-ld+sd-jwt';
   return getCredential(privateKey, byteSigner, mediaType, messageJson);
 };
 
@@ -119,10 +118,10 @@ export const getBinaryMessage = async (
     },
   };
   switch(messageType) {
-    case 'application/vc+sd-jwt': {
+    case 'application/vc-ld+sd-jwt': {
       return getCredential(privateKey, byteSigner, messageType, messageJson);
     }
-    case 'application/vp+sd-jwt': {
+    case 'application/vp-ld+sd-jwt': {
       return getPresentation(privateKey, byteSigner, messageType, messageJson);
     }
     default: {
@@ -140,7 +139,7 @@ export const getSdJwtExample = async (
   const type = Array.isArray(messageJson.type) ?
     messageJson.type : [messageJson.type];
   const messageType = type.includes('VerifiableCredential') ?
-    'application/vc+sd-jwt' : 'application/vp+sd-jwt';
+    'application/vc-ld+sd-jwt' : 'application/vp-ld+sd-jwt';
   const binaryMessage =
     await getBinaryMessage(privateKey, messageType, messageJson);
   const message = new TextDecoder().decode(binaryMessage);
